3bf60dfdd4944d363245ef1d5b46c32fff12b93d
Robert Chen <robertchen234@gmail.com>
2018-10-02 20:02:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,7 @@
 # Your code goes here!
 class Dog 
-  def initialize(name)
-    @name = name  
-  end
-  
-  def name
-    @name
-  end
-  
-  def name=(new_name)
-    @name = new_name
-  end
   
   def bark
     puts "woof!"
   end
 end
-
-Dog.name
-Dog.bark
